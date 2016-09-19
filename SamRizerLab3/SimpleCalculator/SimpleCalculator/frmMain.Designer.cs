3306b3a42eb8dbbc40400b5a81164df844d148f3
sxmrxzxr <tue95606@temple.edu>
2016-09-18 22:44:43 -0400

    finishing touches on lab #3

---
+++ SamRizerLab3/SimpleCalculator/SimpleCalculator/frmMain.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SimpleCalculator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperator = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operand 1: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Operator: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Operand 2:";
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(172, 10);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(100, 20);
+            this.txtOperand1.TabIndex = 3;
+            // 
+            // txtOperator
+            // 
+            this.txtOperator.Location = new System.Drawing.Point(172, 46);
+            this.txtOperator.Name = "txtOperator";
+            this.txtOperator.Size = new System.Drawing.Size(100, 20);
+            this.txtOperator.TabIndex = 4;
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(172, 82);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(100, 20);
+            this.txtOperand2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Result: ";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(172, 120);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 7;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCalculate.Location = new System.Drawing.Point(35, 163);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(163, 163);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(284, 213);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperator);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.TextBox txtOperator;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ SamRizerLab3/SimpleCalculator/SimpleCalculator/frmMain.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        
+        private bool IsPreset(TextBox t, string name)
+        {
+            if (t.Text == "")
+            {
+                MessageBox.Show(name + " is a required field");
+                t.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsDecimal(TextBox t, string name)
+        {
+            decimal num = 0m;
+
+            if (Decimal.TryParse(t.Text, out num))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " is a required field.");
+                t.Focus();
+                return false;
+            }
+        }
+
+        private bool IsWithinRange(TextBox t, string name, decimal min, decimal max)
+        {
+            decimal num = Convert.ToDecimal(t.Text);
+
+            if (num < min || num > max)
+            {
+                MessageBox.Show(name + " must be between " + min.ToString() + " and " + max.ToString() + ".");
+                t.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private decimal Calculate(decimal o1, decimal o2, string o)
+        {
+            switch (o)
+            {
+                case "+":
+                    return o1 + o2;
+                case "-":
+                    return o1 - o2;
+                case "*":
+                    return o1 * o2;
+                case "/":
+                    return Math.Round(o1 / o2, 4);
+                default:
+                    return 0; //error
+            }
+        }
+
+        private bool IsValidData()
+        {
+            if (!IsPreset(txtOperand1, "Operand 1"))
+                return false;
+            if (!IsDecimal(txtOperand1, "Operand 1"))
+                return false;
+            if (!IsWithinRange(txtOperand1, "Operand 1", 1, 100000))
+                return false;
+
+            if (!IsPreset(txtOperand2, "Operand 2"))
+                return false;
+            if (!IsDecimal(txtOperand2, "Operand 2"))
+                return false;
+            if (!IsWithinRange(txtOperand2, "Operand 2", 1, 1000000))
+                return false;
+
+            if (!IsPreset(txtOperator, "Operator"))
+                return false;
+            if (!IsDecimal(txtOperator, "Operator"))
+                return false;
+            if (!IsWithinRange(txtOperator, "Operator", 1, 1000))
+                return false;
+
+            return true;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal operand1 = Convert.ToDecimal(txtOperand1.Text);
+                decimal operand2 = Convert.ToDecimal(txtOperand2.Text);
+                string operater = Convert.ToString(txtOperator.Text);
+
+                if (IsValidData())
+                {
+                    txtResult.Text = Calculate(operand1, operand2, operater).ToString();
+                    txtOperand1.Focus();
+                } 
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Please enter a valid input, " + ex.StackTrace);
+                txtOperand1.Clear();
+                txtOperand2.Clear();
+                txtOperator.Clear();
+                txtOperand1.Focus();
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show("No, " + ex.StackTrace);
+                txtOperand1.Clear();
+                txtOperand2.Clear();
+                txtOperator.Clear();
+                txtOperand1.Focus();
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("Do not divide by zero, " + ex.StackTrace);
+                txtOperand2.Clear();
+                txtOperand2.Focus();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
