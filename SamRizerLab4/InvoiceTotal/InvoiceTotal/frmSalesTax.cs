9bb67c26585dcd4b0e5559ca730c3b1f5ba3c2c0
sxmrxzxr <tue95606@temple.edu>
2016-10-02 19:18:23 -0400

    completed 10-3

--- SamRizerLab4/InvoiceTotal/InvoiceTotal/frmSalesTax.cs
+++ SamRizerLab4/InvoiceTotal/InvoiceTotal/frmSalesTax.cs
@@ -18,10 +18,67 @@ namespace InvoiceTotal
             InitializeComponent();
         }
 
+        public bool IsValidData()
+        {
+            return
+                IsPresent(txtInput, "Input") &&
+                IsDecimal(txtInput, "Input") &&
+                IsWithinRange(txtInput, "Input", 0, 10);
+        }
+
+        public bool IsPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsDecimal(TextBox textBox, string name)
+        {
+            decimal number = 0m;
+            if (Decimal.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a decimal number.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+
+        public bool IsWithinRange(TextBox textBox, string name, decimal min, decimal max)
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number <= min || number >= max)
+            {
+                MessageBox.Show(name + " must be between " + min +
+                    " and " + max + ".", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
         private void btnOK_Click(object sender, EventArgs e)
         {
-            decimal salesTax = Convert.ToDecimal(txtInput.Text);
-            Tag = salesTax;
+
+            if (IsValidData())
+            {
+                decimal salesTax = Convert.ToDecimal(txtInput.Text);
+                Tag = salesTax;
+            }
+            else
+            {
+                txtInput.Clear();
+                txtInput.Focus();
+            }
+            //Tag = salesTax;
         }
     }
 }
