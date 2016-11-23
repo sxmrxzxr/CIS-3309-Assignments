4a08fbc55c7cc2e96d1d8808f734a78af1e3757f
sxmrxzxr <tue95606@temple.edu>
2016-11-23 13:25:10 -0500

    finished some productlist methods

--- ProductRWRizerS/ProductRWRizerS/ProductList.cs
+++ ProductRWRizerS/ProductRWRizerS/ProductList.cs
@@ -22,7 +22,7 @@ namespace ProductRWRizerS
 
         public void readFromFile(string filename)
         {
-            StreamReader read = new StreamReader(File.OpenRead(@"Products.csv"));
+            StreamReader read = new StreamReader(File.OpenRead(filename));
 
             while (!read.EndOfStream)
             {
@@ -50,7 +50,6 @@ namespace ProductRWRizerS
                         this.Add(new Music(values[0], values[1], Convert.ToDouble(values[2]), Convert.ToInt32(values[3]), values[4], Convert.ToDateTime(values[5]), Convert.ToInt32(values[6]), Convert.ToInt32(values[7]), values[8], TimeSpan.Parse(values[9]), values[10], values[11], values[12]));
                         break;
                 }
-                //this.Add(new )
             }
         }
 
@@ -69,7 +68,82 @@ namespace ProductRWRizerS
 
         public void ToCSV()
         {
+            StreamWriter writer = new StreamWriter(@"Output.csv");
 
+            foreach (Product p in this)
+            {
+                writer.WriteLine(p.ToCSV());              
+            }            
+        }
+
+        public void writeToBinary()
+        {
+            BinaryWriter bOut = new BinaryWriter(new FileStream(@"Products.dat", FileMode.Create, FileAccess.Write));
+
+            foreach (Product p in this)
+            {
+                bOut.Write(p.Desc);
+                bOut.Write(p.ID);
+                bOut.Write(p.Price);
+                bOut.Write(p.Quantity);
+                bOut.Write(p.Type);
+
+                switch (p.Desc)
+                {
+                    case "DressShirt":
+                        DressShirt d = (DressShirt)p;
+                        bOut.Write(d.Color);
+                        bOut.Write(d.Manufacturer);
+                        bOut.Write(d.Material);
+                        bOut.Write(d.Neck);
+                        bOut.Write(d.Sleeve);
+                        break;
+                    case "Pants":
+                        Pants x = (Pants)p;
+                        bOut.Write(x.Color);
+                        bOut.Write(x.Manufacturer);
+                        bOut.Write(x.Material);
+                        bOut.Write(x.Inseam);
+                        bOut.Write(x.Waist);
+                        break;
+                    case "TShirt":
+                        TShirt t = (TShirt)p;
+                        bOut.Write(t.Color);
+                        bOut.Write(t.Manufacturer);
+                        bOut.Write(t.Material);
+                        bOut.Write(t.Size);
+                        break;
+                    case "Software":
+                        Software s = (Software)p;
+                        bOut.Write(s.ReleaseDate.ToString());
+                        bOut.Write(s.NumDisks);
+                        bOut.Write(s.Size);
+                        bOut.Write(s.TypeDisk);
+                        bOut.Write(s.TypeSoft);
+                        break;
+                    case "Movie":
+                        Movie m = (Movie)p;
+                        bOut.Write(m.ReleaseDate.ToString());
+                        bOut.Write(m.NumDisks);
+                        bOut.Write(m.Size);
+                        bOut.Write(m.TypeDisk);
+                        bOut.Write(m.RunTime.ToString());
+                        bOut.Write(m.Director);
+                        bOut.Write(m.Producer);
+                        break;
+                    case "Music":
+                        Music u = (Music)p;
+                        bOut.Write(u.ReleaseDate.ToString());
+                        bOut.Write(u.NumDisks);
+                        bOut.Write(u.Size);
+                        bOut.Write(u.TypeDisk);
+                        bOut.Write(u.RunTime.ToString());
+                        bOut.Write(u.Artist);
+                        bOut.Write(u.Genre);
+                        bOut.Write(u.Label);
+                        break;
+                }
+            }
         }
     }
     
